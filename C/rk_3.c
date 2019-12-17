965de7688ad1a0307ce9dfd427f2549ca29ce195
Arkady <insite1029@gmail.com>
2019-12-17 19:50:50 +0300

    Update rk_3.c

--- C/rk_3.c
+++ C/rk_3.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void input();
+
+typedef struct List{
+    int digit;
+    struct List *next;
+{ List;
+
 int main(int argc, char const *argv[])
 {
-    
+    input();
 
     return 0;
+}
+
+void input()
+{
+    
 }
