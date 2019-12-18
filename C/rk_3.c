0e2dda902f707f5d8db3e8debdaa92d783574619
Arkady <insite1029@gmail.com>
2019-12-18 21:35:40 +0300

    C_code

--- C/rk_3.c
+++ C/rk_3.c
@@ -6,30 +6,30 @@ typedef struct List{
     struct List *next;
 } List;
 
-List *first = NULL;
-List *pl = NULL;
+List *first = NULL;                     // создаем указатель на первый узел списка
+List *pl = NULL;                        // создаем указатель на последний узел списка
 
-void fill_in(int);
-void search(void);
-void output(void);
+void fill_in(int);                      // ф-ия принимает считанное из файла число и вносит его в список
+void search(void);                      // ф-ия ищет числа, которые > 10, и удаляет их из списка
+void output(void);                      // ф-ия выводит измененный список
 
 int main(int argc, char* argv[])
 {
     int digit, num_f;
     List *pf;
     
-    pf = (List *) malloc( sizeof(List) );
+    pf = (List *) malloc( sizeof(List) );           // создаем и заполняем новый узел; first становится указателем на него
     
     pf -> num = 100;
     pf -> next = NULL;
     first = pf;
     
-    pl = first;
+    pl = first;                                     // последний узел сначала указывает на первый
     
     FILE *file;    
     file = fopen(argv[1], "r");
     
-    while( fscanf(file, "%d", &num_f ) != EOF )
+    while( fscanf(file, "%d", &num_f ) != EOF )     // считываем числа из файла
     {
         digit = num_f;
         
@@ -46,25 +46,25 @@ void fill_in(int digit)
 {
     List *pw;
     
-    pw = (List *) malloc( sizeof(List) );
+    pw = (List *) malloc( sizeof(List) );           // создаем новый узел и заполняем его
     
     pw -> num = digit;
     pw -> next = NULL;
     pl -> next = pw;
-    pl = pw;
+    pl = pw;                                        // pl сдвигается в конец списка
 }
 
 void search(void)
 {
     List *pf, *pb, *pn;
     
-    pf = first;
+    pf = first;                                     // pf - указател на первый элемент списка
     
     while( pf != NULL )
     {
         if( (pf -> num) > 10 )
         {
-            if( (pf -> next) == (first -> next) )
+            if( (pf -> next) == (first -> next) )   // сдвиг первого узла, если первое число > 10
             {
                 first = first -> next;
                 
@@ -92,10 +92,6 @@ void search(void)
 
 void output(void)
 {   
-    List *pf;
-    
-    pf = first;
-    
     while( first -> next != NULL )
     {
         printf("%d\n", first -> num);
