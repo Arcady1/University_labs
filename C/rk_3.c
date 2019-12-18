bade42d19fdf3aa517c87f24267174be268de85f
Arkady <insite1029@gmail.com>
2019-12-18 21:04:14 +0300

    works_but_no_free() - new

--- C/rk_3.c
+++ C/rk_3.c
@@ -6,25 +6,25 @@ typedef struct List{
     struct List *next;
 } List;
 
-List first;
-List *pl = &first;
+List first;                         // создаем пустой список (внешняя переменная)
+List *pl = &first;                  // создаем укзатель на последний узел списка (внешняя переменная)
 
-void fill_in(int);
-void search(void);
-void output(void);
+void fill_in(int);                  // ф-ия принимает считанное из файла число и вносит его в список
+void search(void);                  // ф-ия ищет числа, которые > 10, и удаляет их из списка
+void output(void);                  // ф-ия выводит измененный список
 
 int main(int argc, char* argv[])
 {
     int digit, num_f;
     
-    first.num = 100;
+    first.num = 100;                // заполняем внешний узел first значениями
     first.next = NULL;
     
     FILE *file;
     
     file = fopen(argv[1], "r");    
     
-    while( fscanf(file, "%d", &num_f ) != EOF )
+    while( fscanf(file, "%d", &num_f ) != EOF )     // считываем числа из файла
     {
         digit = num_f;
         
@@ -41,11 +41,11 @@ void fill_in(int digit)
 {
     List *pw;
     
-    pw = (List *) malloc( sizeof(List) );
+    pw = (List *) malloc( sizeof(List) );           // создаем новый узел и заполняем
     
     pw -> num = digit;
     pw -> next = NULL;
-    pl -> next = pw;
+    pl -> next = pw;                                // pl сдвигается в конец списка 
     pl = pw;
 }
 
@@ -53,7 +53,7 @@ void search(void)
 {
     List *pf, *pb, *pn, *p_first;
     
-    pf = &first;
+    pf = &first;                                    // pf - указатель на первый узел списка
     
     while( pf != NULL )
     {
@@ -91,13 +91,10 @@ void output(void)
     
     while( a -> next != NULL )
     {
-        printf("=======\n");
         printf("%d\n", a -> num);
         
         a = a -> next;
     }
     
-    printf("=======\n");
     printf("%d\n", a -> num);
-    printf("=======\n");
 }
