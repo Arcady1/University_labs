5f7040ce7667a946498a347baa996e1e19debf5c
Arkady <insite1029@gmail.com>
2020-02-24 14:05:13 +0300

    24.02.20
    no_array

--- C++/lab_1/main.cc
+++ C++/lab_1/main.cc
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
+
+class Point
+{
+    private:
+        int x;
+        int y;
+        Point(int _x, int _y) : x(_x), y(_y) { };
+    public:
+        int getx() { return x; };
+        int gety() { return y; };
+        Point() { x=y=0.0; };
+
+        float distance (int, int);
+};
+
+float Point::distance (int X, int Y)
+{
+    float r = sqrt(X*X + Y*Y);
+
+    return r;
+}
 
 int main(int argc, char const *argv[])
 {
-    cout << "Hello, world!" << endl;
+    int *x, *y;
+
+    sscanf(argv[1], "(%d)", &x);
+
+    //cout << x.getx() << endl;
 
     return 0;
 }
