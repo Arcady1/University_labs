fa282f9a8fe4cc7725c0cabf17018aea6527ef42
Arkady <insite1029@gmail.com>
2020-02-24 15:46:42 +0300

    Updated_24.02.20

--- C++/lab_1/main.cc
+++ C++/lab_1/main.cc
@@ -1,35 +1,75 @@
+// Гусаров Аркадий РК6-23Б 1 курс. Программа поиска точки любого заданного набора точек на плоскости, которая наиболее удалена от начала координат
+
 #include <iostream>
 #include <math.h>
 using namespace std;
 
 class Point
 {
-    private:
-        int x;
-        int y;
-        Point(int _x, int _y) : x(_x), y(_y) { };
-    public:
-        int getx() { return x; };
-        int gety() { return y; };
-        Point() { x=y=0.0; };
+private:
+    int x;
+    int y;
 
-        float distance (int, int);
+public:
+    // конструктор по умолчанию
+    Point() { x = y = 0; }
+
+    Point(int valueX, int valueY)
+    {
+        x = valueX;
+        y = valueY;
+    }
+
+    // геттеры - для получения координат x, y
+    int GetX() { return x; };
+    int GetY() { return y; };
+
+    double distance()
+    {
+        double r = sqrt(x*x + y*y);
+
+        return r;
+    }
 };
-
-float Point::distance (int X, int Y)
-{
-    float r = sqrt(X*X + Y*Y);
-
-    return r;
-}
 
 int main(int argc, char const *argv[])
 {
-    int *x, *y;
+    int i = 0;
+    int j = 0;
+    int X, Y;
+    double R = 0.0;
+    double current_distance;
+    Point *current_point;
+    Point **p;                                          // массив указателей
 
-    sscanf(argv[1], "(%d)", &x);
 
-    //cout << x.getx() << endl;
+    // проверка, что введены координаты хотя бы одной точки
+    if ( argc < 2 ) { return -1; }
+
+    p = new Point *[argc];                              // массив указателей размером argc
+
+    while (++i < argc)
+    {
+        sscanf(argv[i], "%d%*c%d", &X, &Y);
+
+        p[j] = new Point(X, Y);
+        j++;
+    }
+
+    p[j] = NULL;                                        // инициализация последенй ячейки массива указателей
+
+    for ( i = 0; p[i] != NULL; i++ )
+    {
+        current_point = p[i];                           // указатель на i-ые координаты (чтобы посмотреть значение X: 'cout << current_point->GetX() << endl;')
+        current_distance = current_point->distance();
+
+        if ( current_distance > R )
+            R = current_distance;
+        
+        cout << current_point->GetX() << "\t";
+        cout << current_point->GetY() << "\t";
+        cout << current_point->distance() << endl;
+    }
 
     return 0;
 }
