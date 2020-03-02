08420d54173b6fef1da5f1a33d22953f3a45aea8
Arkady <insite1029@gmail.com>
2020-03-02 11:48:43 +0300

    Updated_02.03.20
    Реализовал инициализацию массива нулями; вывод массива

--- C++/lab_2/main.cc
+++ C++/lab_2/main.cc
@@ -1,34 +1,64 @@
 // Гусаров Аркадий РК6-23Б 1 курс. Программа построения магического квадрата любого нечетного порядка из последовательных натуральных чисел по методу Москопула.
 
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 // создаю класс Magic - магический квадрат
 class Magic
 {
     private:
-        int row;                        // строка
-        int col;                        // столбец
-        int N;                          // размерность массива
-        unsigned** tab;                 // указатель на первый указатель массива указателей
+        int row;                            // строка
+        int col;                            // столбец
+        int N;                              // размерность массива
+        unsigned** tab;                     // указатель на первый указатель массива указателей
     public:
-        Magic(int dim) : N(dim) {};     // конструктор инициализации размерности массива
-        ~Magic();                       // деструктор
-        void Fill_in(int);              // метод заполнения магического квадрата
-        void Print();                   // метод вывода магического квадрата на экран
+        Magic(int dim) : N(dim) {};         // конструктор инициализации размерности массива
+        //~Magic();                           // деструктор
+        void Create();
+        void Fill_in();                  // метод заполнения магического квадрата
+        void Print();                       // метод вывода магического квадрата на экран
 };
 
-void Magic::Fill_in(int N)
+void Magic::Create()
+{
+    tab = new unsigned* [N];                // tab присваиваем указатель на первый указатель массива указателей
+
+    for (row = 0; row < N; row++)           // создаем N строк
+        tab[row] = new unsigned [N];
+        
+    for (row = 0; row < N; row++)
+        for (col = 0; col < N; col++)       // создаем N столбоц
+            tab[row][col] = 0;              // инициализируем матрицу нулями
+}
+
+void Magic::Fill_in()
 {
 
+}
+
+void Magic::Print()
+{
+    for (row = 0; row < N; row++)
+    {
+        for (col = 0; col < N; col++)
+        {
+            printf("%d\t", tab[row][col]);
+        }
+        printf("\n");
+    }
 }
 
 int main(int argc, char const *argv[])
 {
     int N;
+
     N = atoi(argv[1]);              // задаем размернность массива через аргумент командной строки
 
     Magic magic(N);                 // создаем объект класса Magic
+    magic.Create();
+    magic.Fill_in();
+    magic.Print();
 
     return 0;
 }
