4805cff176a4f6de6f78afd1ecccf74267f727ae
Arkady <insite1029@gmail.com>
2020-03-02 23:25:03 +0300

    Updated_02.03.20
    Программа сделана, но неправильно ставится число, если ячейка занята - нет переменных для старых координат

--- C++/test.cc
+++ C++/test.cc
@@ -16,7 +16,7 @@ class Magic                                                 // создаю кл
         //~Magic();                                           // деструктор
         void Create();                                      // метод создания N - мерного массива и заполнения нулями
         void Fill_in();                                     // метод заполнения магического квадрата
-        void Check_for_free(unsigned, int, int);            // в методе реализован алгоритм размещения числа в матрице, в зависимости от условия: клетка пустая / непустая
+        void Check_for_free(unsigned);            // в методе реализован алгоритм размещения числа в матрице, в зависимости от условия: клетка пустая / непустая
         void Print();                                       // метод вывода магического квадрата на экран
 };
 
@@ -37,74 +37,85 @@ void Magic::Fill_in()
     unsigned current_digit;                                 // текущее число
     unsigned max_digit;                                     // максимально возможное число
     unsigned n;                                             // счетчик (позволяет не выходить за границы матрицы)
-    int i, j;                                               // координаты 'current_digit'
 
     current_digit = 1;
     max_digit = N * N;
     n = N - 1;
-    i = n;
-    j = n/2;
-    tab[i][j] = current_digit;                              // расположение единицы в матрице (низ, центр)
+    row = n;
+    col = n/2;
+    tab[row][col] = current_digit;                              // расположение единицы в матрице (низ, центр)
 
     for (current_digit = 2; current_digit != max_digit; current_digit++)
     {
-        i -= 2;
-        j -= 1;
+        row -= 2;
+        col -= 1;
 
-        if ( ((i >=0) & (i <= n)) & ((j >=0) & (j <= n)) )
-            Check_for_free(current_digit, i, j);
+        printf("Maybe: row = %d; col = %d; digit = %d\n", row, col, current_digit);
+
+        if ( ((row >=0) & (row <= n)) & ((col >=0) & (col <= n)) )
+            Check_for_free(current_digit);
         else
         {
-            if ( i >= 0 )
+            if ( row >= 0 )
             {
-                j += N;
-                Check_for_free(current_digit, i, j);
+                col += N;
+                Check_for_free(current_digit);
             }
-            else if ( j >= 0 )
+            else if ( col >= 0 )
             {
-                i += N;
-                Check_for_free(current_digit, i, j);
+                row += N;
+                Check_for_free(current_digit);
             }
             else
             {
-                i += N;
-                j += N;
-                Check_for_free(current_digit, i, j);
+                row += N;
+                col += N;
+                Check_for_free(current_digit);
             }
         }
     }    
 }
 
-void Magic::Check_for_free(unsigned cur_digit, int i, int j)
+void Magic::Check_for_free(unsigned cur_digit)
 {
-    Print();
-    printf("\ni = %d; j = %d; digit = %d\n", i, j, cur_digit);
+    printf("Will be: row = %d; col = %d; digit = %d\n", row, col, cur_digit);
 
-    if ( tab[i][j] == 0 )
-        tab[i][j] = cur_digit;
+    if ( tab[row][col] == 0 )
+    {
+        tab[row][col] = cur_digit;
+        
+        printf("NOW:\n");
+        Print();
+    }
     else
     {
-        printf("@");
-        if ( i <= 4 )
+        if ( row <= 4 )
         {
-            i += N - 4;
-            tab[i][j] = cur_digit;
+            row += N - 4;
+            tab[row][col] = cur_digit;
         }                    
         else
         {
-            i -= 4;
-            tab[i][j] = cur_digit;
-        }                    
+            row -= 4;
+            tab[row][col] = cur_digit;
+        }
+
+        printf("NOW:\n");
+        Print();
+        printf("\n");
     }
 }
 
 void Magic::Print()
 {
-    for (row = 0; row < N; row++)
+    int i;
+    int j;
+
+    for (i = 0; i < N; i++)
     {
-        for (col = 0; col < N; col++)
+        for (j = 0; j < N; j++)
         {
-            printf("%d\t", tab[row][col]);
+            printf("%d\t", tab[i][j]);
         }
         printf("\n");
     }
