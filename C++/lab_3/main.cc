9c9b2f2f50a55402984b698955abbaa7e6a68dd8
Arkady <insite1029@gmail.com>
2020-03-09 14:26:12 +0300

    Updated_09.03.20
    Реализовал инициализацию массива с кординатами точек; вывод массива

--- C++/lab_3/main.cc
+++ C++/lab_3/main.cc
@@ -1,20 +1,47 @@
+#include <stdio.h>
 #include <iostream>
 using namespace std;
 
 class Point
 {
-    private:
-        int x;
-        int y;
-    public:
-        Point( int x_, int y_ ) : x(x_), y(y_) {};
-        int getX() { return x; };
-        int getY() { return y; };
+private:
+    int x;                                          // координаты точек
+    int y;
+public:
+    Point( int x_, int y_ ) : x(x_), y(y_) {};      // конструктор инициализации списком
+    //friend int distance_x(void);                    // дружественная ф-ия; принимает ..., возвращает расстояние между парами точек
+    //void distance_y (void);                         // метод класса; принимает ..., возвращает ...
+    int getX() { return x; };
+    int getY() { return y; };
 };
+
+// int distance_x(void)
+// {
+
+//     return 1;
+// }
 
 int main(int argc, char const *argv[])
 {
-    
+    Point** tab;                                    // указатель на первый указатель массива указателей
+    int N;                                          // кол-во введенных точек
+    int X, Y;                                       // координаты точек
+
+    N = argc - 1;
+    tab = new Point* [N];                           // массив указателей размерностью N
+
+    for ( int i = 0; i < N; i++ )
+    {
+        sscanf(argv[i + 1], "%d%*c%d", &X, &Y);     // считываем координаты
+
+        tab[i] = new Point (X, Y);                  // записываем координаты в массив
+    }
+
+    // вывод координат точек на экран
+    for ( int i = 0; i < N; i++ )
+    {
+        printf("%d: (%d; %d)\n", i + 1, tab[i]->getX(), tab[i]->getY());
+    }
 
     return 0;
 }
--- C++/test.cc
+++ C++/test.cc
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <iostream>
 using namespace std;
 
@@ -8,17 +9,39 @@ private:
     int y;
 public:
     Point( int x_, int y_ ) : x(x_), y(y_) {};      // конструктор инициализации списком
-    friend int distance_x(void);                    // дружественная ф-ия; принимает ..., возвращает расстояние между парами точек
-    void distance_y (void);                         // метод класса; принимает ..., возвращает ...
+    //friend int distance_x(void);                    // дружественная ф-ия; принимает ..., возвращает расстояние между парами точек
+    //void distance_y (void);                         // метод класса; принимает ..., возвращает ...
     int getX() { return x; };
     int getY() { return y; };
 };
 
+// int distance_x(void)
+// {
+
+//     return 1;
+// }
+
 int main(int argc, char const *argv[])
 {
+    Point** tab;                                    // указатель на первый указатель массива указателей
     int N;                                          // кол-во введенных точек
+    int X, Y;                                       // координаты точек
 
     N = argc - 1;
+    tab = new Point* [N];                           // массив указателей размерностью N
+
+    for ( int i = 0; i < N; i++ )
+    {
+        sscanf(argv[i + 1], "%d%*c%d", &X, &Y);     // считываем координаты
+
+        tab[i] = new Point (X, Y);                  // записываем координаты в массив
+    }
+
+    // вывод координат точек на экран
+    for ( int i = 0; i < N; i++ )
+    {
+        printf("%d: (%d; %d)\n", i + 1, tab[i]->getX(), tab[i]->getY());
+    }
 
     return 0;
 }
