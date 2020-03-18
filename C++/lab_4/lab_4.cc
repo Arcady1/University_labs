a6f8121475ce0c8b0dee6c9d2f38afa290b634ef
Arkady <insite1029@gmail.com>
2020-03-18 11:23:06 +0300

    Updated_18.03.20
    Реализован конструктор инициализации строки и преобразования слова в 'word', заполнение 'alpha' алфавитом заглавных букв

--- C++/lab_4/lab_4.cc
+++ C++/lab_4/lab_4.cc
@@ -4,11 +4,11 @@ using namespace std;
 class Letters
 {
     private:
-        unsigned word;                                          // кодировка слова
+        unsigned word;                                          // кодировка слова (только заглавные буквы)
         unsigned alpha;                                         // кодировка алфавита заглавных букв
     public:
         Letters() { word = 0; alpha = 0; };                     // конструктор по умолчанию
-        Letters(char*);                                         // конструктор инициализации строки и преобразования слова в 'word'; принимает слово
+        Letters(char*);                                         // конструктор инициализации строки и преобразования слова в 'word', заполнение 'alpha' алфавитом заглавных букв; принимает введенное слово
         int operator,(unsigned&);                               // перегрузка оператора ',' для перемножения 'word' и 'alpha'; принимает введенное слово, возвращает число заглавных букв в слове
         friend ostream& operator<<(ostream&, Letters&);         // перегрузка оператора '<<'
         unsigned getAlpha() { return alpha; };
@@ -16,12 +16,29 @@ class Letters
 
 Letters::Letters(char* s)
 {
+    int code;
+
+    word = 0;
+    alpha = 0;
+
+    while (*s)                                          // инициализация единицами позиции заглавных букв введенного слова
+    {
+        code = (int)(*s);
+
+        if ( ((code > 64) & (code < 91)) )
+            word |= (1 << code);
+        
+        s++;
+    }
+
+    for (int i = 65; i < 91; i++)                       // заполнение 'alpha' алфавитом заглавных букв
+        alpha |= (1 << i);
     
 }
 
 int Letters::operator,(unsigned& alph)
 {
-
+    
 }
 
 ostream& operator<<(ostream& out, Letters& word)
