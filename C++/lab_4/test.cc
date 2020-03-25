ede380b79ec6c77dd1e7f50c13eb654890e9a671
Arkady <insite1029@gmail.com>
2020-03-25 14:47:17 +0300

    Updated_25.03.20
    Не работает корректно

--- C++/lab_4/test.cc
+++ C++/lab_4/test.cc
@@ -4,35 +4,32 @@ using namespace std;
 class Letters
 {
     private:
-        unsigned word;                                          // кодировка слова (только заглавные буквы)
-        unsigned alpha;                                         // кодировка алфавита заглавных букв
+        unsigned word;                                          // закодированное слово (из терминала)
+        unsigned alpha;                                         // закодированный алфавит
     public:
         Letters() { word = 0; alpha = 0; };                     // конструктор по умолчанию
-        Letters(char*, unsigned&);                              // конструктор инициализации строки и преобразования слова в 'word', заполнение 'alpha' алфавитом заглавных букв; принимает введенное слово
+        Letters(char*);                                         // конструктор инициализации строки и преобразования слова в 'word', заполнение 'alpha' алфавитом заглавных букв; принимает введенное слово
+
         int operator,(unsigned&);                               // перегрузка оператора ',' для перемножения 'word' и 'alpha'; принимает введенное слово, возвращает число заглавных букв в слове
-        friend ostream& operator<<(ostream&, Letters&);         // перегрузка оператора '<<'
+        // friend ostream& operator<<(ostream&, Letters&);         // перегрузка оператора '<<'
 };
 
-Letters::Letters(char* s, unsigned& alp)
+Letters::Letters(char* s)
 {
     int code;
 
     word = 0;
-    alp = 0;
 
-    while (*s)                                          // инициализация единицами позиции заглавных букв введенного слова
+    while (*s)                                          // инициализация единицами позиции букв введенного слова
     {
         code = (int)(*s);
+        word |= (1 << code);
 
-        if ( ((code > 64) & (code < 91)) )
-            word |= (1 << code);
-        
         s++;
     }
 
     for (int i = 65; i < 91; i++)                       // заполнение 'alpha' алфавитом заглавных букв
-        alp |= (1 << i);
-    
+        alpha |= (1 << i);
 }
 
 int Letters::operator,(unsigned& alp)
@@ -40,6 +37,7 @@ int Letters::operator,(unsigned& alp)
     unsigned bin;
     int count;
     
+    alp = alpha;
     bin = 0;
     count = 0;
 
@@ -53,25 +51,29 @@ int Letters::operator,(unsigned& alp)
             ++count;
     }
 
+    printf("%d\n", count);
+
+    exit (1);
+
     return count;
 }
 
-ostream& operator<<(ostream& out, Letters& wd)
-{
-    unsigned bin;
+// ostream& operator<<(ostream& out, Letters& wd)
+// {
+//     unsigned bin;
 
-    bin = 0;
+//     bin = 0;
 
-    for (int i = 65; i < 91; i++)
-    {
-        bin = 1 << i;
+//     for (int i = 65; i < 91; i++)
+//     {
+//         bin = 1 << i;
 
-        if ( (wd.word & bin) > 0 )
-            out<<(char)(i);        
-    }
+//         if ( (wd.word & bin) > 0 )
+//             out<<(char)(i);        
+//     }
 
-    return out;
-}
+//     return out;
+// }
 
 void Errors_check(int);                                 // ф-ия проверки на ошибки при передаче аргументов; принимает кол-во аргументов
 
@@ -79,15 +81,14 @@ int main(int argc, char *argv[])
 {
     Errors_check(argc);
 
-    unsigned alphabet;
+    unsigned alp;
     int result;
 
-    Letters text(argv[1], alphabet);
+    Letters text(argv[1]);
+    result = (text.operator,(alp));
 
-    result = (text, alphabet);                             // перегрузка оператора ','
-
-    cout << text << endl;
-    cout << result << endl;
+    // cout << text << endl;
+    // cout << result << endl;
 
     return 0;
 }
