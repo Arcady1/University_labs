7523f576998cdd93b8dedd59fea41147395ca175
Arkady <insite1029@gmail.com>
2020-04-09 14:10:39 +0300

    Updated_09.04.20

--- C++/lab_4/lab_4.cc
+++ C++/lab_4/lab_4.cc
@@ -8,10 +8,9 @@ using namespace std;
 class Letters
 {
     private:
-        unsigned low;                                           // находятся строчные буквы слова
-        unsigned cap;                                           // находятся заглавные буквы слова
+        unsigned bin[2];                                        // массив с заглавными и строчными буквами слова
     public:
-        Letters() { low = 0; cap = 67108863; };                 // конструктор по умолчанию
+        Letters() { bin[0] = 0; bin[1] = 67108863; };           // конструктор по умолчанию
         Letters(char*);                                         // конструктор инициализации строки и преобразования слова в 'word', заполнение 'alpha' алфавитом заглавных букв; принимает введенное слово
         int operator,(Letters&);                                // перегрузка оператора ',' для перемножения 'word' и 'alpha'; принимает введенное слово, возвращает число заглавных букв в слове
         friend ostream& operator<<(ostream&, Letters&);         // перегрузка оператора '<<'
@@ -21,18 +20,18 @@ Letters::Letters(char* s)
 {
     int code;
     
-    cap = 0;
-    low = 0;
+    bin[1] = 0;                                         // заглавные буквы слова
+    bin[0] = 0;                                         // строчные буквы слова
 
-    while (*s)                                          // заполнение cap и low 
+    while (*s)                                          // заполнение bin[1] и bin[0] 
     {
         code = (int)(*s);
 
         if ( isupper(*s) )                              // если буква прописная
-            cap |= (1 << (code - 'A'));
+            bin[1] |= (1 << (code - 'A'));
 
         else
-            low |= (1 << (code - 'a')); 
+            bin[0] |= (1 << (code - 'a')); 
 
         s++;
     }
@@ -40,19 +39,19 @@ Letters::Letters(char* s)
 
 int Letters::operator,(Letters& object_)
 {
-    unsigned bin;
+    unsigned mult;
     unsigned digit;
     int count;
 
     count = 0;
     digit = 1;
-    bin = cap & object_.cap;
+    mult = bin[1] & object_.bin[1];                     // в mult заглавные буквы слова без повторений
 
     for (int i = 0; i < 26; i++)                        // подсчет заглавных букв
     {
         digit = 1 << i;
 
-        if ( ((bin & digit) > 0) )
+        if ( ((mult & digit) > 0) )
             count++;
     }
 
@@ -67,7 +66,7 @@ ostream& operator<<(ostream& out, Letters& text_)
 
     for (int i = 0; i < 26; i++)
     {
-        if ( (text_.cap & bin) > 0 )
+        if ( (text_.bin[1] & bin) > 0 )
             out << (char)('A' + i);
         
         bin = bin << 1;
