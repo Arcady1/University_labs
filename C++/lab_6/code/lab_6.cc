f017719ad2e6c6a6e370e8894b97df564c6074a9
Arkady <insite1029@gmail.com>
2020-04-28 21:14:22 +0300

    Updated_28.04.20
    Start

---
+++ C++/lab_6/code/lab_6.cc
@@ -0,0 +1,21 @@
+// Гусаров Аркадий РК6-23Б 1 курс. Программа ...
+// Пример ввода: ./a.out ...
+
+#include <cstdio>
+#include <cstdlib>
+#include "dlink.hpp"
+using namespace std;
+
+class SymLink : public Dlink
+{
+private:
+    
+public:
+    
+}
+
+int main(int argc, char *argv[])
+{
+    /* code */
+    return 0;
+}
