57a2033b15f82e5fac7e725ea8fd56bb88743773
Arkady <insite1029@gmail.com>
2020-05-06 12:26:11 +0300

    Updated_06.05.20
    Кол-во дней между датами

--- C++/lab_7/lab_7.c
+++ C++/lab_7/lab_7.c
@@ -7,10 +7,11 @@ int days_lD(int[], int[]); // ф-ия проверки, сколько дней 
 
 int main(int argc, char *argv[])
 {
-    int i, years, between_years;
+    int i, years, days;
     int date_1[3] = {0};
     int date_2[3] = {0};
     int month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    days = 0;
 
     // считывание дат в массивы
     for (i = 0; i < 2; i++)
@@ -30,18 +31,10 @@ int main(int argc, char *argv[])
 
     // кол-во дней между датами (исключая их самих)
     years = (date_2[2] - 1) - date_1[2];
-    between_years = (365 * years) + leap_year(date_2[2], date_1[2]);
+    days = (365 * years) + leap_year(date_2[2], date_1[2]);
 
-    days_fD(date_1, month);
-    days_lD(date_2, month);
-
-    // printf("%d ", date_1[0]);
-    // printf("%d ", date_1[1]);
-    // printf("%d\n", date_1[2]);
-
-    // printf("%d ", date_2[0]);
-    // printf("%d ", date_2[1]);
-    // printf("%d\n", date_2[2]);
+    days += days_fD(date_1, month);
+    days += days_lD(date_2, month);
 
     return 0;
 }
@@ -72,15 +65,30 @@ int days_fD(int date_1[], int month[])
     // кол-во дней от текущего месяца до конца года
     for (i = date_1[0]; i < 12; i++)
         days += month[i];
-    
+
     // если указанный месяц янв. - фев. и год - високосный, то прибавляем 1 день
     if ((date_1[0] <= 2) & ((leap_year(date_1[2], date_1[2])) == 1))
         days++;
 
-    printf("%d\n", days);
-    exit(100);
+    return days;
 }
 
 int days_lD(int date_2[], int month[])
 {
+    int days, i;
+
+    days = 0;
+    i = 0;
+
+    // кол-во дней от начала месяца до конечного числа 
+    days = date_2[1];
+    // кол-во дней от начала года до текущего месяца
+    for (i = 0; i < date_2[0] - 1; i++)
+        days += month[i];
+
+    // если месяц > фев. и год - високосный, то прибавляем 1 день
+    if ((date_2[0] > 2) & ((leap_year(date_2[2], date_2[2])) == 1))
+        days++;
+
+    return days;
 }
