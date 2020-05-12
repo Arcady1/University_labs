11618f5e1c7f963ccf1a6a636fa9fd28e25601b1
Arkady <insite1029@gmail.com>
2020-05-12 18:31:30 +0300

    Updated_12.05.20

---
+++ C++/lab_7/code/chess.cpp
@@ -0,0 +1,25 @@
+// main file
+/* Вывести шахматную доску, на которой будет базовая фигура, Plus, Cros или
+Sultan в заданной позиции. Позиции под атакой фигуры должны быть обозначены *,
++, x в соответствии с фигурой. */
+
+#include <iostream>
+#include "chesslib.hpp"
+
+int main(int argc, char **argv)
+{
+  // проверка числа аргументов и допустимости позиции
+  if (argc < 2)
+  {
+    std::cout << "Incorrect argument" << std::endl;
+    return 1;
+  }
+  if (Figure::deskout(argv[1]))
+  { // вызов статического метода (sic!)
+    std::cout << "Incorrect position " << argv[1] << std::endl;
+    return 1;
+  }
+  Sultan f(argv[1]); // создание фигуры
+  f.printBoard();  // (sic!)
+  return 0;
+}
