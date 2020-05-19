dad4e2af0e1bcf866b05d269cf34be68864559d3
Arkady <insite1029@gmail.com>
2020-05-19 22:44:54 +0300

    Updated_19.05.20

--- C++/RK_2/RK_2.cc
+++ C++/RK_2/RK_2.cc
@@ -4,7 +4,7 @@
 #include <stdlib.h> // для функции atoi()
 #include <string.h> // для функции strchr()
 #include <math.h>   // для использования модуля
-#include <stdio.h>
+#include <stdio.h>  // для функции printf()
 #include <iostream>
 using namespace std;
 
@@ -18,7 +18,6 @@ public:
     Fraction(int num_, int denom_) : num(num_), denom(denom_){}; // констрктор со списком инициализации
     Fraction(char *);                                            // конструктор инициализации строки
     Fraction operator+(Fraction &);                              // перегрузка оператора '+' для сложения дробей
-    Fraction operator=(Fraction &);                              // перегрузка оператора присваивания ('=')
     void reduce(int &);                                          // метод сокращения дроби
     int evclid();                                                // метод, реализующий алгоритм Евклида
     void output();                                               // метод вывода ответа
@@ -69,16 +68,6 @@ Fraction Fraction::operator+(Fraction &second)
     return result;
 };
 
-Fraction Fraction::operator=(Fraction &result)
-{
-    printf("@\n");
-    // int sumNum, sumDenom;
-    // sumNum = this->num * second.denom;
-
-    // sumDenom =
-    // this->num =
-};
-
 int Fraction::evclid()
 {
     int a, b, t;
@@ -112,48 +101,51 @@ void Fraction::output()
         numer = abs(this->num);
         negative = true;
     }
-    //
     else
         numer = this->num;
 
     if (denomer == 1)
     {
         if (negative)
-            printf("Итог:\t\t-%d\n", numer);
-
+            printf("Результат:\t-%d\n", numer);
         else
-            printf("Итог:\t\t%d\n", numer);
+            printf("Результат:\t%d\n", numer);
     }
 
     else if (numer > denomer)
     {
         if (negative)
-            printf("Итог:\t\t-%d ", numer / denomer);
-
+            printf("Результат:\t-%d ", numer / denomer);
         else
-            printf("Итог:\t\t%d ", numer / denomer);
+            printf("Результат:\t%d ", numer / denomer);
 
         this->num = numer % denomer;
         printf("%d/%d\n", this->num, this->denom);
     }
 };
 
-void Errors(int); // функция проверки на ошибки при вводе аргументов
+void Errors(int, Fraction, Fraction); // функция проверки на ошибки
 
 int main(int argc, char *argv[])
 {
     int gcd;
 
-    Errors(argc);
-
     Fraction first = argv[1];
     Fraction second = argv[2];
+
+    Errors(argc, first, second);
 
     printf("Вы ввели:\t%d/%d\n", first.getNum(), first.getDenom());
     printf("\t\t%d/%d\n", second.getNum(), second.getDenom());
 
     Fraction sum = first + second;
     printf("Сумма:\t\t%d/%d\n", sum.getNum(), sum.getDenom());
+
+    if (sum.getNum() == 0)
+    {
+        printf("Итог:\t\t%d\n", sum.getNum());
+        return 1;
+    }
 
     // поиск НОД
     gcd = sum.evclid();
@@ -168,11 +160,17 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void Errors(int argc)
+void Errors(int argc, Fraction first, Fraction second)
 {
     if (argc != 3)
     {
         printf("Ошибка при вводе аргументов\n");
         exit(1);
+    }
+    
+    if ((first.getDenom() == 0) || (second.getDenom() == 0))
+    {
+        printf("0 в знаменателе\n");
+        exit(2);
     }
 }
