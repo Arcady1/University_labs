f7a952cca7fc809ee3bce275ba7ac5886a956f23
Arcady1 <insite1029@gmail.com>
2021-12-06 23:33:05 +0300

    lab4

---
+++ CCM/lab_4/recursive.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printVector(vector<int> vec)
+{
+    for (int i = 0; i < vec.size() - 1; i++)
+        cout << vec[i] << " + ";
+    cout << vec[vec.size() - 1] << endl;
+}
+
+vector<int> startVectorGenerator(int n, int m)
+{
+    vector<int> arr;
+    arr.resize(m);
+
+    for (int i = 0; i < m; i++)
+        arr[i] = 1;
+    arr[arr.size() - 1] = n - (m - 1);
+
+    return arr;
+}
+
+int Gitenburg(vector<int> vec, int n, int elIdx)
+{
+    if (elIdx == -1)
+        elIdx = vec.size() - 2;
+
+    vec[vec.size() - 1] -= 1;
+    vec[elIdx] += 1;
+    elIdx -= 1;
+
+    if (vec[vec.size() - 1] < vec[vec.size() - 2])
+        return 0;
+
+    printVector(vec);
+    Gitenburg(vec, n, elIdx);
+}
+
+int main(int argc, const char *argv[])
+{
+    int n, m;
+
+    cout << "Введите целое число больше 0: ";
+    cin >> n;
+    cout << "Введите количество слагаемых: ";
+    cin >> m;
+
+    if ((n * m == 0) || (n < 0) || (m < 0) || (m > n))
+    {
+        cout << "Ошибка ввода" << endl;
+        return 1;
+    }
+
+    if (m == 1)
+    {
+        cout << n << endl;
+        return 0;
+    }
+
+    vector<int> vec = startVectorGenerator(n, m);
+    printVector(vec);
+
+    return Gitenburg(vec, n, vec.size() - 2);
+}
